5cc4fda5f28d8a4a94f558927cf53668449c570d
Oleksandr Sokoliuk <technosanya@gmail.com>
2019-02-24 17:11:04 +0100

    Update 8544.c

--- PCD8544/8544.c
+++ PCD8544/8544.c
@@ -1,7 +1,6 @@
 //#define F_CPU 8000000UL
 #include "8544.h"
 
-
 void lcd_spi_send(uint8_t dc, uint8_t data){
 	
 	if (dc == LCD_DATA){
@@ -9,7 +8,6 @@ void lcd_spi_send(uint8_t dc, uint8_t data){
 	} else {
 		PCD8544_PORT &=~(1<<PIN_DC);
 	}
-	
 	PCD8544_PORT &=~(1<<PIN_CE);
 	for (uint8_t i = 0;i<8; i++){
 		PCD8544_PORT = (data & 0x80) ? PCD8544_PORT | (1<<PIN_DIN) : PCD8544_PORT & ~(1<<PIN_DIN);
@@ -28,12 +26,34 @@ void lcd_init(void){
 	lcd_spi_send(LCD_ORDER,0x04);
 	lcd_spi_send(LCD_ORDER,0x14);	
 	lcd_spi_send(LCD_ORDER,0x20);
-	lcd_spi_send(LCD_ORDER,0x0C);	
-	
+	lcd_spi_send(LCD_ORDER,0x0C);
 }
 void lcd_clear(void){
 	int i;
 	for(i=0;i < LCD_X * LCD_Y /8; i++){
 		lcd_spi_send(LCD_DATA,0x00);
+	}
+}
+
+void lcd_set_cursor(uint8_t x, uint8_t y){
+	 x=x%12; y=y%6;
+	 lcd_spi_send(LCD_ORDER,0x40+y*7);
+	 lcd_spi_send(LCD_ORDER,0x80+x*7);	 	 
+}
+void lcd_send_string(char *str, uint8_t x, uint8_t y){
+	lcd_set_cursor(x,y);
+	while(*str){
+		lcd_char(*str++);
+	}
+}
+
+void lcd_char(uint8_t char1){
+	if(char1 >= 0x20 && char1 <= 0x80){
+		int i;
+		lcd_spi_send(LCD_DATA, 0x00);
+		for(i=0;i<5;i++){
+			lcd_spi_send(LCD_DATA, ASCII[char1 - 0x20][i]);
+		}
+		lcd_spi_send(LCD_DATA, 0x00);
 	}
 }
