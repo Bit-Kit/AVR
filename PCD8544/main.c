c965b3553a2da2ca9adcf83f4671f1d556adb352
Oleksandr Sokoliuk <technosanya@gmail.com>
2019-02-24 17:09:29 +0100

    Update main.c

--- PCD8544/main.c
+++ PCD8544/main.c
@@ -3,16 +3,17 @@
     #include "8544.h"
 #endif
 
+// 12x6 symbols LCD
 int main(void){
 	lcd_init();
 	lcd_clear();
-	lcd_spi_send(LCD_DATA, 0x7f);
-	lcd_spi_send(LCD_DATA, 0x09);
-	lcd_spi_send(LCD_DATA, 0x19);
-	lcd_spi_send(LCD_DATA, 0x29);
-	lcd_spi_send(LCD_DATA, 0x46);	
 	for (;;){
-	_delay_ms(500);	
+		// your string, position x, position y
+		lcd_send_string("I am a man",0,0); 
+		lcd_send_string("  who walks",0,1);
+		lcd_send_string("       alone",0,2); 
+		lcd_send_string("IRON MAIDEN",0,5); 
+		_delay_ms(500);	
 	}
 	return 0;
 }
