b1bb2aea38b2a4561888b3b34b5087c15f1a4a39
Oleksandr Sokoliuk <technosanya@gmail.com>
2019-07-28 13:23:58 +0200

    Update i2c.h

--- SSD1306/i2c.h
+++ SSD1306/i2c.h
@@ -1,10 +1,10 @@
-﻿#ifndef I2C_H_
+#ifndef I2C_H_
 #define I2C_H_
 #include <avr/io.h>
 //========<>========<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>========<>========//
-void I2C_Initialization(void); 						//Inicjalizacja I2C
-void I2C_Start(void); 								//Wysyłka sekwencji START
-void I2C_Stop(void); 								//Wysyłka sekwencji STOP
-void I2C_Send_BYTE(unsigned char c); 				//Wysyłka bajtu na magistrale
+void I2C_Initialization(void); 						        //Inicjalizacja I2C
+void I2C_Start(void); 								            //Wysyłka sekwencji START
+void I2C_Stop(void); 								              //Wysyłka sekwencji STOP
+void I2C_Send_BYTE(unsigned char c); 			      	//Wysyłka bajtu na magistrale
 //========<>========<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>========<>========//
 #endif
