3bf85fbb6d31b7ba1cb48da062e6784a9ddf82ed
Oleksandr Sokoliuk <technosanya@gmail.com>
2019-07-28 13:23:30 +0200

    Update i2c.c

--- SSD1306/i2c.c
+++ SSD1306/i2c.c
@@ -1,25 +1,25 @@
-﻿#define F_CPU 8000000UL
-#define F_SCL 100000L										//Prędkość transmisji = 100 kHz
+#define F_CPU 8000000UL
+#define F_SCL 100000L					 //Prędkość transmisji = 100 kHz
 #include "i2c.h"
 #include <avr/io.h>
 //========<>========<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>========<>========//
-void I2C_Initialization (void){								//Inicjalizacja TWI
-	TWBR = ((F_CPU/F_SCL)-8)/2;								//Zapis do rejestru przędkości transmisji migistrali 0x20
-	TWSR = 0;												//Ustawienie preskalera na zero
+void I2C_Initialization (void){				 //Inicjalizacja TWI
+	TWBR = ((F_CPU/F_SCL)-8)/2;			 //Zapis do rejestru przędkości transmisji migistrali 0x20
+	TWSR = 0;				 	 //Ustawienie preskalera na zero
 }
 //========<>========<*>=======<*>=======<*>=======<*>=======<*>=======<*>========<>========//
-void I2C_Start (void){ 										//Wysyłka sekwencji START
+void I2C_Start (void){ 					 //Wysyłka sekwencji START
 	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));							//Oczekiwanie na zmianę flagi TWINT
+	while (!(TWCR & (1<<TWINT)));			 //Oczekiwanie na zmianę flagi TWINT
 }
 //========<>========<*>=======<*>=======<*>=======<*>=======<*>=======<*>========<>========//
-void I2C_Stop (void){										//Wysyłka sekwencji STOP
+void I2C_Stop (void){					 //Wysyłka sekwencji STOP
 	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
 }
 //========<>========<*>=======<*>=======<*>=======<*>=======<*>=======<*>========<>========//
-void I2C_Send_BYTE(unsigned char c){ 						//Wysyłka bajtu na magistrale
-	TWDR = c;												//Zapisywanie bajtu danych do rejestru
+void I2C_Send_BYTE(unsigned char c){ 			 //Wysyłka bajtu na magistrale
+	TWDR = c;					 //Zapisywanie bajtu danych do rejestru
 	TWCR = (1<<TWINT)|(1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));							//Oczekiwanie na zmianę flagi TWINT
+	while (!(TWCR & (1<<TWINT)));			 //Oczekiwanie na zmianę flagi TWINT
 }
 //========<>========<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>=======<*>========<>========//
